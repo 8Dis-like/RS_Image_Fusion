c7b348607fe3d6ffb5115cdb4b17c24900df83b4
Zugho <91202795+8Dis-like@users.noreply.github.com>
2023-01-05 19:50:21 +0800

    Weighed Fusion
    The function RGBfusion works well, for further application.

---
+++ HighPass/Methods.h
@@ -0,0 +1,14 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include<time.h>
+
+using namespace cv;
+using namespace std;
+
+//将RGB图像融合为彩色图像并对比度增强
+Mat RGBfusion(string,string,string);
+
+void Contraststretch(Mat& M);
+void Contraststretch(Mat& M, int minout, int maxout);
+void weighing(Mat&, Mat&);
+
--- Methods.h
+++
@@ -1,6 +0,0 @@
-#include <opencv2/opencv.hpp>
-#include <iostream>
-#include<time.h>
-
-using namespace cv;
-using namespace std;
